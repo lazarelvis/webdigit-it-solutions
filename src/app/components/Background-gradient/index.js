03753017654dbfa721d6b57846fc5063255ddbb8
lazarelvis <lazarelvis15@gmail.com>
2025-04-18 21:02:25 +0300

    chanced background js script to react script and now the state of background persists

--- src/app/components/Background-gradient/index.js
+++ src/app/components/Background-gradient/index.js
@@ -1,14 +1,39 @@
+"use client";
+import { useEffect } from "react";
+
 export default function Backgroundgradient() {
+  useEffect(() => {
+    const loadGradient = async () => {
+      // Dynamically load the script only once
+      const script = document.createElement("script");
+      script.src =
+        "https://cdn.jsdelivr.net/gh/greentfrapp/pocoloco@minigl/minigl.js";
+      script.async = true;
+
+      script.onload = () => {
+        // @ts-ignore because "Gradient" is a global added by the script
+        if (typeof Gradient !== "undefined") {
+          const gradient = new Gradient();
+          gradient.initGradient("#canvas");
+        }
+      };
+
+      document.body.appendChild(script);
+
+      // Optional: cleanup script on unmount
+      return () => {
+        document.body.removeChild(script);
+      };
+    };
+
+    loadGradient();
+  }, []);
+
   return (
     <div>
       <div className="background--custom gradient">
-        {/* <!-- aici ajustes cand-e sus sau jos sa se duca gradient-ul --> */}
         <canvas id="canvas" className="h-9/20 sm:h-3/5" />
       </div>
-      <script src="https://cdn.jsdelivr.net/gh/greentfrapp/pocoloco@minigl/minigl.js"></script>
-      <script>
-        var gradient = new Gradient(); gradient.initGradient("#canvas");
-      </script>
     </div>
   );
 }
