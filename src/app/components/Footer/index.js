d14862011668dcbb219dcfc69eaee04b05f63c74
lazarelvis <lazarelvis15@gmail.com>
2025-04-30 11:39:32 +0300

    added link footer terms and conditiions

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -45,7 +45,22 @@ export default function Footer() {
   return (
     <div className="w-full bg-[#0a2540] text-red-500 py-y ">
       <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-[#f6f9fc]">
-        <p className="py-4">Toate drepturile rezervate &#169; 2025 Webdigit</p>
+        <div>
+          <p className="py-4">
+            Toate drepturile rezervate &#169; 2025 Webdigit
+          </p>
+          <div className="flex text-sm font-medium">
+            <p className="pr-5">
+              <Link href="/termeni-si-conditii">Termenii și condiții</Link>
+            </p>
+            <p>
+              <Link href="/politica-de-confidentialitate">
+                Politica de confidențialitate
+              </Link>
+            </p>
+          </div>
+          <p></p>
+        </div>
         <div className="flex justify-between sm:w-[200px] pt-4 text-2xl">
           {items.map((x, index) => {
             return (
