ec3b83d3d8a501fe009ac958c28f2f446e66d90d
lazarelvis <lazarelvis15@gmail.com>
2025-05-07 01:50:44 +0300

    Changed priority image to flalse to an increase in loading perfomance

--- src/app/components/Portoflio/ui/porfolio-card.js
+++ src/app/components/Portoflio/ui/porfolio-card.js
@@ -72,7 +72,7 @@ export default function PortfolioCard() {
                 alt={item.alt}
                 fill
                 style={{ objectFit: "cover" }}
-                priority
+                priority={false}
               />
             </div>
             {/* h-50 is the hight of the card */}
@@ -82,7 +82,7 @@ export default function PortfolioCard() {
                 alt={item.alt}
                 fill
                 style={{ objectFit: "cover" }}
-                priority
+                priority={false}
               />
             </div>
           </div>
